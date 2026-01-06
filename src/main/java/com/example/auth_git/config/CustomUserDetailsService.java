e39dad443bb297476996bbdd7627b39e6fc47d1d
teklu <teklufa@gmail.com>
2026-01-06 16:28:55 +0300

    add custom user details service

---
+++ src/main/java/com/example/auth_git/config/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.auth_git.config;
+
+import com.example.auth_git.exception.RecordExistsException;
+import com.example.auth_git.user.UserRepository;
+import com.example.auth_git.user.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RecordExistsException("record not found."));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRole()))
+                .build();
+    }
+}
