e7cb5bbb2bbeaf476f13fb8d74afd987061c2583
teklu <teklufa@gmail.com>
2026-01-06 11:28:58 +0300

    Repository, Service, Constroller and Openapi
     - create repo, service and controller for user
     - add openapi dependency to activate swagger

---
+++ src/main/java/com/example/auth_git/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.auth_git.user;
+
+import com.example.auth_git.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/example/auth_git/user/UserService.java
@@ -0,0 +1,7 @@
+package com.example.auth_git.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
